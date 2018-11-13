b76f52978449996aaaada4d0aa401e173b9e0147
Brian Cipriano <brian.cipriano@gmail.com>
2018-11-13 11:15:50 -0800

    Python implementation of FrameSet/FrameRange. (#22)

---
+++ spi_cue/FileSequence/FrameSet.py
@@ -0,0 +1,65 @@
+#  Copyright (c) 2018 Sony Pictures Imageworks Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from FrameRange import FrameRange
+
+
+class FrameSet(object):
+    """Represents a sequence of FrameRanges."""
+
+    def __init__(self, frameRange):
+        """Construct a FrameSet object by parsing a spec.
+
+        See FrameRange for the supported syntax. A FrameSet follows the same syntax,
+        with the addition that it may be a comma-separated list of different FrameRanges.
+        """
+        self.frameList = self.parseFrameRange(frameRange)
+
+    def __str__(self):
+        # TODO(cipriano) Make this smarter, group frame ranges and by step. (b/119317272)
+        return ','.join([str(frame) for frame in self.frameList])
+
+    def size(self):
+        """Gets the number of frames contained in this sequence."""
+        return len(self.frameList)
+
+    def get(self, idx):
+        """Gets an individual entry in the sequence, by numerical position."""
+        return self.frameList[idx]
+
+    def index(self, idx):
+        """Query index of frame number in frame set.
+
+        Returns:
+            int, index of frame. -1 if frame set does not contain frame.
+        """
+        try:
+            return self.frameList.index(idx)
+        except ValueError:
+            return -1
+
+    def getAll(self):
+        """Gets the full numerical sequence."""
+        return self.frameList
+
+    def normalize(self):
+        self.frameList = list(set(self.frameList))
+        self.frameList.sort()
+
+    @staticmethod
+    def parseFrameRange(frameRange):
+        frameList = list()
+        for frameRangeSection in frameRange.split(','):
+            frameList.extend(FrameRange.parseFrameRange(frameRangeSection))
+        return frameList
