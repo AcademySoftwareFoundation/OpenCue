4e2bd036579d7315a1a7ad89045f9d10db6bb540
Brian Cipriano <cipriano@google.com>
2018-12-04 13:07:25 -0800

    Remove Oracle migrations

--- cue3bot/src/main/resources/conf/ddl/oracle/migrations/update_2009_05_27.sql
+++
@@ -1,5 +0,0 @@
-/**
-* Add some columns to store virtual memory in use.
-**/
-ALTER TABLE proc ADD int_virt_used NUMERIC(16,0) DEFAULT 0 NOT NULL;
-ALTER TABLE proc ADD int_virt_max_used NUMERIC(16,0) DEFAULT 0 NOT NULL;
