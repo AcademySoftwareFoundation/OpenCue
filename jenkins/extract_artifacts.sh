27dd8b27f86c3d2731b372c51c52ff193666cff7
Brian Cipriano <brian.cipriano@gmail.com>
2019-01-15 14:44:15 +0100

    Standardize packaging to use /opt/opencue. (#107)

--- jenkins/extract_artifacts.sh
+++ jenkins/extract_artifacts.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <BUILD_ID> <ARTIFACT_DIRECTORY>"
   exit 1
@@ -18,18 +20,18 @@ mkdir -p "$artifact_directory"
 echo "{\"git_commit\": \"${GIT_COMMIT}\"}" | tee "${artifact_directory}/build_metadata.json"
 
 container_id=$(docker create opencue/cuebot:${build_id})
-docker cp $container_id:/opt/cue3/cuebot-${build_id}-all.jar "$artifact_directory/"
+docker cp $container_id:/opt/opencue/cuebot-${build_id}-all.jar "$artifact_directory/"
 docker rm $container_id
 
 container_id=$(docker create opencue/rqd:${build_id})
-docker cp $container_id:/opt/cue3/rqd-${build_id}-all.tar.gz "$artifact_directory/"
+docker cp $container_id:/opt/opencue/rqd-${build_id}-all.tar.gz "$artifact_directory/"
 docker rm $container_id
 
 container_id=$(docker create opencue/pycue:${build_id})
-docker cp $container_id:/opt/cue3/pycue-${build_id}-all.tar.gz "$artifact_directory/"
+docker cp $container_id:/opt/opencue/pycue-${build_id}-all.tar.gz "$artifact_directory/"
 docker rm $container_id
 
 container_id=$(docker create opencue/cuegui:${build_id})
-docker cp $container_id:/opt/cue3/cuegui-${build_id}-all.tar.gz "$artifact_directory/"
+docker cp $container_id:/opt/opencue/cuegui-${build_id}-all.tar.gz "$artifact_directory/"
 docker rm $container_id
 
