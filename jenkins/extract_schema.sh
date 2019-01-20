c6117ca015d2d92c5e978ff9714998bac3df8a68
Brian Cipriano <cipriano@google.com>
2019-01-20 15:29:13 +0100

    use container IP

--- jenkins/extract_schema.sh
+++ jenkins/extract_schema.sh
@@ -12,17 +12,20 @@ ARTIFACT_DIRECTORY=$2
 
 DB_USER=postgres
 DB_NAME=cuebot_extract_$BUILD_ID
-HOST_PORT=$(shuf -i 10000-20000 -n 1)
+# HOST_PORT=$(shuf -i 10000-20000 -n 1)
 PG_CONTAINER=postgres-$BUILD_ID
 SCHEMA_DIRECTORY="$(pwd)/cuebot/src/main/resources/conf/ddl/postgres"
 
 docker pull postgres
 docker pull boxfuse/flyway
-docker run --rm --name $PG_CONTAINER -d -p $HOST_PORT:5432 postgres
+docker run --rm --name $PG_CONTAINER -d postgres
 sleep 10
+PG_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $PG_CONTAINER)
 docker exec -t --user=$DB_USER $PG_CONTAINER createdb $DB_NAME
-docker run --rm -v "${SCHEMA_DIRECTORY}/migrations:/flyway/sql" boxfuse/flyway -url=jdbc:postgresql://localhost:$HOST_PORT/$DB_NAME -user=$DB_USER migrate
+docker run --rm -v "${SCHEMA_DIRECTORY}/migrations:/flyway/sql" boxfuse/flyway -url=jdbc:postgresql://$PG_IP/$DB_NAME -user=$DB_USER migrate
 docker exec -t --user=$DB_USER $PG_CONTAINER pg_dump --no-privileges --no-owner -s cuebot_extract | tee "${ARTIFACT_DIRECTORY}/schema-${BUILD_ID}.sql"
 
 cp "${SCHEMA_DIRECTORY}/demo_data.sql" "${ARTIFACT_DIRECTORY}/demo_data-${BUILD_ID}.sql"
+
+# docker kill $PG_CONTAINER
 
