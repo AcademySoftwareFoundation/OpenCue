76a4913f197c39984039dfa998ad18113c72120b
Brian Cipriano <cipriano@google.com>
2019-07-16 17:55:23 -0700

    Move Cuebot JAR extraction to build step

--- ci/extract_artifacts.sh
+++ ci/extract_artifacts.sh
@@ -10,7 +10,7 @@ fi
 build_id=$1
 artifact_directory=$2
 
-if [[ -z "${GIT_COMMIT}" ]]; then
+if [ -z "${GIT_COMMIT}" ]; then
   print 'Environment var GIT_COMMIT must be set.'
   exit 1
 fi
@@ -21,9 +21,9 @@ cp LICENSE "$artifact_directory"
 
 echo "{\"git_commit\": \"${GIT_COMMIT}\"}" | tee "${artifact_directory}/build_metadata.json"
 
-container_id=$(docker create opencue/cuebot:${build_id})
-docker cp $container_id:/opt/opencue/cuebot-${build_id}-all.jar "$artifact_directory/"
-docker rm $container_id
+#container_id=$(docker create opencue/cuebot:${build_id})
+#docker cp $container_id:/opt/opencue/cuebot-${build_id}-all.jar "$artifact_directory/"
+#docker rm $container_id
 
 #for component in rqd pycue pyoutline cuegui cuesubmit cueadmin; do
 #  container_id=$(docker create opencue/${component}:${build_id})
