2f9e21bfa7780b8bdb39be0bd48efaf9cc01ccdc
Brian Cipriano <cipriano@google.com>
2019-07-16 19:07:11 -0700

    Fix typo, clean out some old files

--- ci/extract_artifacts.sh
+++ ci/extract_artifacts.sh
@@ -11,7 +11,7 @@ build_id=$1
 artifact_directory=$2
 
 if [ -z "${BUILD_SOURCEVERSION}" ]; then
-  print 'Environment var GIT_COMMIT must be set.'
+  print 'Environment var BUILD_SOURCEVERSION must be set.'
   exit 1
 fi
 
@@ -20,14 +20,3 @@ mkdir -p "$artifact_directory"
 cp LICENSE "$artifact_directory"
 
 echo "{\"git_commit\": \"${BUILD_SOURCEVERSION}\"}" | tee "${artifact_directory}/build_metadata.json"
-
-#container_id=$(docker create opencue/cuebot:${build_id})
-#docker cp $container_id:/opt/opencue/cuebot-${build_id}-all.jar "$artifact_directory/"
-#docker rm $container_id
-
-#for component in rqd pycue pyoutline cuegui cuesubmit cueadmin; do
-#  container_id=$(docker create opencue/${component}:${build_id})
-#  docker cp $container_id:/opt/opencue/${component}-${build_id}-all.tar.gz "$artifact_directory/"
-#  docker rm $container_id
-#done
-
