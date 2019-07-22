da204d5eee1c1d0a29ebe143e86272887cb7dd11
Brian Cipriano <cipriano@google.com>
2019-07-22 11:59:31 -0700

    Build pipeline improvements (#383)

--- ci/extract_artifacts.sh
+++ ci/extract_artifacts.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-if [ "$#" -ne 2 ]; then
+if [[ "$#" -ne 2 ]]; then
   echo "Usage: $0 <BUILD_ID> <ARTIFACT_DIRECTORY>"
   exit 1
 fi
@@ -10,7 +10,7 @@ fi
 build_id=$1
 artifact_directory=$2
 
-if [ -z "${BUILD_SOURCEVERSION}" ]; then
+if [[ -z "${BUILD_SOURCEVERSION}" ]]; then
   echo 'Environment var BUILD_SOURCEVERSION must be set.'
   exit 1
 fi
