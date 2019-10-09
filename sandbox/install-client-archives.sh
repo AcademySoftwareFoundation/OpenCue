53cdcccc3217ae32baff6265666f3c95814262b6
Sharif Salah <sharifsalah@users.noreply.github.com>
2019-10-09 11:49:06 +0100

    Update sandbox client packages install script (#468)
    * Add an alternative client install script for the sandbox
    
    Signed-off-by: Sharif Salah <sharifsalah@google.com>
    
    * Update sandbox README
    
    Signed-off-by: Sharif Salah <sharifsalah@google.com>
    
    * Rename install-clients.sh
    
    Signed-off-by: Sharif Salah <sharifsalah@google.com>
    
    * Update the client archive script to check multiple download URLs
    
    Signed-off-by: Sharif Salah <sharifsalah@google.com>
    
    * Fix typo
    
    Signed-off-by: Sharif Salah <sharifsalah@google.com>
    
    * Update script instructions
    
    Signed-off-by: Sharif Salah <sharifsalah@google.com>
    
    * Remove the redirect from an echo statement for consistency
    
    Signed-off-by: Sharif Salah <sharifsalah@google.com>

---
+++ sandbox/install-client-archives.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# To run this script, you must first set the version number.
+# For example, to set the version to 0.2.31, run:
+# export VERSION=0.2.31
+# For additional requirements, see https://www.opencue.io/docs/quick-starts/
+
+if [[ -z "${VERSION}" ]]; then
+    echo "You must set the release version number. For example:"
+    echo "export VERSION=0.2.31"
+    echo "For a list of OpenCue version numbers, visit the following URL:"
+    echo "https://github.com/AcademySoftwareFoundation/OpenCue/releases/"
+    exit 1
+fi
+
+CLIENT_PACKAGES=( pycue pyoutline cueadmin cuegui cuesubmit  )
+
+BASE_URL=https://github.com/AcademySoftwareFoundation/OpenCue/releases/download/
+
+mkdir opencue-downloads
+
+cd opencue-downloads
+
+for PACKAGE in "${CLIENT_PACKAGES[@]}"; do
+    # older versions of OpenCue provided a slightly different download URL
+    # format.
+    wget ${BASE_URL}${VERSION}/${PACKAGE}-${VERSION}-all.tar.gz \
+        || wget ${BASE_URL}v${VERSION}/${PACKAGE}-${VERSION}-all.tar.gz
+    tar xvzf ${PACKAGE}-${VERSION}-all.tar.gz
+    pip install -r ${PACKAGE}-${VERSION}-all/requirements.txt
+    cd ${PACKAGE}-${VERSION}-all
+    python setup.py install
+    cd ..
+done
+
+cd ..
+
+rm -rf opencue-downloads
