6d333db7c96cd3e90098b8bd81a682d84043a636
Brian Cipriano <cipriano@google.com>
2019-11-20 15:07:54 -0800

    Add separate pipelines for tests and packaging. (#506)

---
+++ ci/run_python_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+pip install --user -r requirements.txt
+
+# Protos need to have their Python code generated in order for tests to pass.
+python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
+python -m grpc_tools.protoc -I=proto/ --python_out=rqd/rqd/compiled_proto --grpc_python_out=rqd/rqd/compiled_proto proto/*.proto
+
+python pycue/setup.py test
+PYTHONPATH=pycue python pyoutline/setup.py test
+PYTHONPATH=pycue python cueadmin/setup.py test
+PYTHONPATH=pycue xvfb-run python cuegui/setup.py test
+PYTHONPATH=pycue:pyoutline python cuesubmit/setup.py test
+python rqd/setup.py test
