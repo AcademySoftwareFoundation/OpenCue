97fd4a29e57a7beff4c78063512dec52a05730e7
Brian Cipriano <cipriano@google.com>
2019-12-20 18:53:20 -0800

    Run testing pipeline against CY 2020. (#587)

--- ci/run_python_tests.sh
+++ ci/run_python_tests.sh
@@ -8,6 +8,10 @@ pip install --user -r requirements.txt -r requirements_gui.txt
 python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
 python -m grpc_tools.protoc -I=proto/ --python_out=rqd/rqd/compiled_proto --grpc_python_out=rqd/rqd/compiled_proto proto/*.proto
 
+# Fix imports to work in both Python 2 and 3. See
+# <https://github.com/protocolbuffers/protobuf/issues/1491> for more info.
+sed -i 's/^\(import.*_pb2\)/from . \1/' pycue/opencue/compiled_proto/*.py rqd/rqd/compiled_proto/*.py
+
 python pycue/setup.py test
 PYTHONPATH=pycue python pyoutline/setup.py test
 PYTHONPATH=pycue python cueadmin/setup.py test
