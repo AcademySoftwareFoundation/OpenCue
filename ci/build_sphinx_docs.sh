e93b14405acb82cf3853448084951cd606452844
Sharif Salah <sharifsalah@google.com>
2019-12-24 11:02:03 +0000

    Update sphinx-build command

--- ci/build_sphinx_docs.sh
+++ ci/build_sphinx_docs.sh
@@ -8,5 +8,7 @@ pip install --user -r requirements.txt -r docs/requirements.txt
 # Must generate Python code from Protos in order for Sphinx to build the docs.
 python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
 
+find . -name sphinx-build -print
+
 # Build the docs and treat warnings as errors
 ./local/bin/sphinx-build -W -b html -d docs/_build/doctrees docs docs/_build/html
