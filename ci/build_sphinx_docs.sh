ca6408ac6edd4316764127e407a2ebd0e85181c7
Sharif Salah <sharifsalah@google.com>
2020-01-17 13:18:43 +0000

    Add Sphinx docs build to testing pipeline

---
+++ ci/build_sphinx_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Sphinx has some additional requirements
+pip install --user -r requirements.txt -r docs/requirements.txt
+
+# Must generate Python code from Protos in order for Sphinx to build the docs.
+python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
+
+# Build the docs and treat warnings as errors
+~/.local/bin/sphinx-build -W -b html -d docs/_build/doctrees docs docs/_build/html
