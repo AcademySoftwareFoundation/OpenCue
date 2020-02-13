826fb8bf2002e545f4481fc38feea187a802be6e
Brian Cipriano <cipriano@google.com>
2020-02-12 16:32:25 -0800

    Fix copyright.

--- cuegui/tests/images/images_tests.py
+++ cuegui/tests/images/images_tests.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2018 Sony Pictures Imageworks Inc.
+#  Copyright (c) OpenCue Project Authors
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
