c480cb114c6ae3fd3a2301fb2883cc14f1a460a4
Diego Tavares da Silva <dtavares@imageworks.com>
2020-07-30 10:37:11 -0700

    Update copyrights to ASWF TAC required format. (#735)

--- pycue/tests/wrappers/service_test.py
+++ pycue/tests/wrappers/service_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#  Copyright (c) 2018 Sony Pictures Imageworks Inc.
+#  Copyright Contributors to the OpenCue Project
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
