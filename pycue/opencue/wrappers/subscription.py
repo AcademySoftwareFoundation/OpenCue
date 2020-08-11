6f4d3c4d6eb89532df1e1761f41737bc4f5c728c
DiegoTavares <dtavares@imageworks.com>
2020-08-11 09:56:34 -0700

    Update subscription wrapper burst typecast
    Use int instead of long

--- pycue/opencue/wrappers/subscription.py
+++ pycue/opencue/wrappers/subscription.py
@@ -44,15 +44,13 @@ class Subscription(object):
         return Subscription(response.subscription)
 
     def setSize(self, size):
-        assert (isinstance(size, int) or
-                isinstance(size, long)), "size is not expected type: int, long"
+        assert (isinstance(size, int)), "size is not expected type: int"
         self.stub.SetSize(
             subscription_pb2.SubscriptionSetSizeRequest(subscription=self.data, new_size=size),
             timeout=Cuebot.Timeout)
 
     def setBurst(self, burst):
-        assert (isinstance(burst, int) or
-                isinstance(burst, long)), "burst is not expected type: int, long"
+        assert (isinstance(burst, int)), "burst is not expected type: int"
         self.stub.SetBurst(
             subscription_pb2.SubscriptionSetBurstRequest(subscription=self.data, burst=burst),
             timeout=Cuebot.Timeout)
