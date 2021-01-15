253035bac6a6a8d48a864905b5e777c97f5bacd3
Brian Cipriano <cipriano@google.com>
2021-01-15 16:51:05 -0500

    Check for files which should not be changed.

--- ci/check_version_bump.py
+++ ci/check_version_bump.py
@@ -40,8 +40,8 @@ def main():
 
     if violating_files and not version_file_updated:
         print(
-            'Files were changed which indicate version incompatibility, but the OpenCue minor '
-            'version number was not updated.')
+            'Files were added or changed which indicate version incompatibility, but the OpenCue '
+            'minor version number was not updated.')
         print('Please update VERSION.in and increment the minor version number.')
         print('Violating files: \n  %s' % '\n  '.join(violating_files))
         sys.exit(1)
