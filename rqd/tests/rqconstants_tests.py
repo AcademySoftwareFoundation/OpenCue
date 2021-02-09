2f43dcd0be83e346cfcb117738dab76b1e276fa0
Kazuki Sakamoto <sakamoto@splhack.org>
2021-02-09 01:08:01 -0800

    Add rqmachine test

--- rqd/tests/rqconstants_tests.py
+++ rqd/tests/rqconstants_tests.py
@@ -24,10 +24,26 @@ import shutil
 import os.path
 import tempfile
 import unittest
+import uuid
+
+import pyfakefs.fake_filesystem_unittest
 
 import six
 
 import rqd.rqconstants
+import rqd.rqcore
+import rqd.rqmachine
+import rqd.rqnimby
+import rqd.rqutil
+import rqd.compiled_proto.report_pb2
+
+from .rqmachine_tests import (
+    CPUINFO,
+    CUDAINFO,
+    LOADAVG_LOW_USAGE,
+    MEMINFO_MODERATE_USAGE,
+    PROC_STAT,
+)
 
 
 if not six.PY2:
@@ -37,9 +53,8 @@ if not six.PY2:
 
 
 class MockConfig(object):
-    def __init__(self, content):
-        self.tempdir = tempfile.mkdtemp()
-        config = os.path.join(self.tempdir, "rqd.conf")
+    def __init__(self, tempdir, content):
+        config = os.path.join(tempdir, str(uuid.uuid4()))
         self.patcher = mock.patch("sys.argv", ["rqd", "-c", config])
 
         with open(config, "w") as f:
@@ -52,7 +67,6 @@ class MockConfig(object):
 
     def __exit__(self, type, value, traceback):
         self.patcher.stop()
-        shutil.rmtree(self.tempdir)
 
     def __call__(self, func):
         def decorator(*args, **kwargs):
@@ -62,21 +76,72 @@ class MockConfig(object):
         return decorator
 
 
-class RqConstantTests(unittest.TestCase):
+@mock.patch("subprocess.getoutput", new=mock.MagicMock(return_value=CUDAINFO))
+@mock.patch.object(
+    rqd.rqutil.Memoize, "isCached", new=mock.MagicMock(return_value=False)
+)
+@mock.patch("platform.system", new=mock.MagicMock(return_value="Linux"))
+@mock.patch("os.statvfs", new=mock.MagicMock())
+@mock.patch(
+    "rqd.rqutil.getHostname", new=mock.MagicMock(return_value="arbitrary-hostname")
+)
+class RqConstantTests(pyfakefs.fake_filesystem_unittest.TestCase):
+
+    tempdir = tempfile.mkdtemp()
+
+    def setUp(self):
+        self.setUpPyfakefs()
+        self.fs.add_real_directory(self.tempdir)
+        self.fs.create_file("/proc/cpuinfo", contents=CPUINFO)
+        self.loadavg = self.fs.create_file("/proc/loadavg", contents=LOADAVG_LOW_USAGE)
+        self.procStat = self.fs.create_file("/proc/stat", contents=PROC_STAT)
+        self.meminfo = self.fs.create_file(
+            "/proc/meminfo", contents=MEMINFO_MODERATE_USAGE
+        )
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    def makeRqMachine(self):
+        rqCore = mock.MagicMock(spec=rqd.rqcore.RqCore)
+        nimby = mock.MagicMock(spec=rqd.rqnimby.Nimby)
+        rqCore.nimby = nimby
+        nimby.active = False
+        nimby.locked = False
+        coreDetail = rqd.compiled_proto.report_pb2.CoreDetail(total_cores=2)
+        machine = rqd.rqmachine.Machine(rqCore, coreDetail)
+
+        machine.renderHost = machine.__dict__["_Machine__renderHost"]
+
+        return machine
+
     @MockConfig(
+        tempdir,
         """
 [Override]
 DEFAULT_FACILITY =  test_facility
-"""
+""",
     )
     def test_facility(self):
         self.assertEqual(rqd.rqconstants.DEFAULT_FACILITY, "test_facility")
 
+        machine = self.makeRqMachine()
+        self.assertEqual(machine.renderHost.facility, "test_facility")
+
     @MockConfig(
+        tempdir,
         """
 [Override]
 RQD_TAGS =  test_tag1 test_tag2  test_tag3
-"""
+""",
     )
     def test_tags(self):
         self.assertEqual(rqd.rqconstants.RQD_TAGS, "test_tag1 test_tag2  test_tag3")
+
+        machine = self.makeRqMachine()
+        self.assertEqual(machine.renderHost.facility, "cloud")
+        self.assertTrue(
+            set(["test_tag1", "test_tag2", "test_tag3"]).issubset(
+                machine.renderHost.tags
+            )
+        )
