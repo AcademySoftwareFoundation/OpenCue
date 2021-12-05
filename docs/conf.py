d41fb46e32001da1bafb09ab32e8bab77d1f8800
Brian Cipriano <cipriano@google.com>
2021-12-05 18:59:44 -0500

    Upgrade Sphinx and theme to fix dependency issue.

--- docs/conf.py
+++ docs/conf.py
@@ -87,14 +87,13 @@ pygments_style = None
 html_theme = 'sphinx_rtd_theme'
 # Logo (optional)
 html_logo = "images/opencue_logo_navbar.png"
-
+html_baseurl = 'https://www.opencue.io/'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'canonical_url': 'https://www.opencue.io/',
     'logo_only': True,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
