10364d147ff4f34baa0a9c2b6608fc7d7e40b18c
dtavares <dtavares@imageworks.com>
2022-03-28 09:53:25 -0700

    Revert import * change

--- cuebot/src/main/java/com/imageworks/spcue/service/JobSpec.java
+++ cuebot/src/main/java/com/imageworks/spcue/service/JobSpec.java
@@ -23,7 +23,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
