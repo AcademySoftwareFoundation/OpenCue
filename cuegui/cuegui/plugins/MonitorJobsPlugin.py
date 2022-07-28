e9959518608e64f95abe9a4112e27760372ae9e7
dtavares <dtavares@imageworks.com>
2022-07-27 18:02:17 -0700

    Fix GUI not updating bugs
     - Check if regex expression is empty before querying
     - Reduce timeout to a workable value. According to (gRPC Python docs)[https://grpc.github.io/grpc/python/grpc.html#grpc.UnaryUnaryMultiCallable.__call__],
    timeout is measured in seconds, meaning the current configuration is 2.7h which doesn't seem useful. Reducing to 10s.
     - Reducing JobMonitor tick to 10s
     - Check if regex expression is empty before querying
     - Fix JobMonitorTree getJobs call

--- cuegui/cuegui/plugins/MonitorJobsPlugin.py
+++ cuegui/cuegui/plugins/MonitorJobsPlugin.py
@@ -199,19 +199,20 @@ class MonitorJobsDockWidget(cuegui.AbstractDockWidget.AbstractDockWidget):
 
         self.jobMonitor.removeAllItems()
 
-        if cuegui.Utils.isStringId(substring):
-            # If a uuid is provided, load it
-            self.jobMonitor.addJob(substring)
-        elif load_finished_jobs or re.search(
-                r"^([a-z0-9_]+)\-([a-z0-9\.]+)\-", substring, re.IGNORECASE):
-            # If show and shot is provided, or if "load finished" checkbox is checked, load all jobs
-            for job in opencue.api.getJobs(regex=[substring], include_finished=True):
-                self.jobMonitor.addJob(job)
-        else:
-            # Otherwise, just load current matching jobs (except for the empty string)
-            if substring:
-                for job in opencue.api.getJobs(regex=[substring]):
-                    self.jobMonitor.addJob(job)
+        if len(substring) > 0:
+            if cuegui.Utils.isStringId(substring):
+                # If a uuid is provided, load it
+                self.jobMonitor.addJob(substring)
+            elif load_finished_jobs or re.search(
+                    r"^([a-z0-9_]+)\-([a-z0-9\.]+)\-", substring, re.IGNORECASE):
+                # If show and shot is provided, or if "load finished" checkbox is checked, load all jobs
+                for job in opencue.api.getJobs(regex=[substring], include_finished=True):
+                        self.jobMonitor.addJob(job)
+            else:
+                # Otherwise, just load current matching jobs (except for the empty string)
+                if substring:
+                    for job in opencue.api.getJobs(regex=[substring]):
+                        self.jobMonitor.addJob(job)
 
     def getGrpDependent(self):
         """Is group dependent checked"""
