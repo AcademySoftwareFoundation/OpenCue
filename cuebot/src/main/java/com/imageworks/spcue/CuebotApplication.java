35aa4612d70fc15c1b2b6a97bde2f0f3aed7a29a
Brian Cipriano <cipriano@google.com>
2022-12-06 12:29:29 -0500

    [cuebot] Automatically replace --log.frame-log-root with the new flag. (#1203)

--- cuebot/src/main/java/com/imageworks/spcue/CuebotApplication.java
+++ cuebot/src/main/java/com/imageworks/spcue/CuebotApplication.java
@@ -16,18 +16,48 @@
  */
 
 
-
 package com.imageworks.spcue;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CuebotApplication extends SpringApplication {
+    private static String[] checkArgs(String[] args) {
+        Optional<String> deprecatedFlag = Arrays.stream(args)
+                .filter(arg -> arg.startsWith("--log.frame-log-root=")).findFirst();
+        if (deprecatedFlag.isPresent()) {
+            // Log a deprecation warning.
+            Logger warning_logger = Logger.getLogger(CuebotApplication.class);
+            warning_logger.warn("`--log.frame-log-root` is deprecated and will be removed in an " +
+                    "upcoming release. It has been replaced with `--log.frame-log-root.default_os`. " +
+                    "See opencue.properties for details on OpenCue's new OS-dependent root directories.");
+            // If new flags are not present, swap in the value provided using the new flag.
+            // If the new flags are already present, don't do anything.
+            Optional<String> newFlags = Arrays.stream(args)
+                    .filter(arg -> arg.startsWith("--log.frame-log-root.")).findAny();
+            if (!newFlags.isPresent()) {
+                String fixedFlag = "--log.frame-log-root.default_os="
+                        + StringUtils.substringAfter(deprecatedFlag.get(), "=");
+                args = Stream.concat(
+                                Arrays.stream(args).filter(arg -> !arg.startsWith("--log.frame-log-root=")),
+                                Stream.of(fixedFlag))
+                        .toArray(String[]::new);
+            }
+        }
+        return args;
+    }
 
     public static void main(String[] args) {
         // Cuebot startup
-        SpringApplication.run(CuebotApplication.class, args);
+        String[] filteredArgs = checkArgs(args);
+        SpringApplication.run(CuebotApplication.class, filteredArgs);
     }
 }
 
