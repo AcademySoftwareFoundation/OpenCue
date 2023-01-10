6cc5e0603878c483f7cea12d14a1b71fabd769c5
Brian Cipriano <cipriano@google.com>
2023-01-10 16:20:44 -0500

    Try stdout

--- ci/run_gui_test.sh
+++ ci/run_gui_test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-stderr_log="/tmp/stderr.log"
-PYTHONPATH=pycue xvfb-run -d python cuegui/setup.py test 2> >(tee ${stderr_log} >&2)
+test_log="/tmp/cuegui_result.log"
+PYTHONPATH=pycue xvfb-run -d python cuegui/setup.py test | tee ${test_log}
 
 echo "#################"
 echo "Done with main test run"
 echo "#################"
 
-cat ${stderr_log}
+ls -l ${test_log}
+cat ${test_log}
 
