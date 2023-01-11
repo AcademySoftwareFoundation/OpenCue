41361a788ce9071af8446f9a65139bd6f0ed4ef7
Brian Cipriano <cipriano@google.com>
2023-01-11 18:59:29 -0500

    Use new action to get changed files list.

--- cuebot/src/main/resources/conf/ddl/postgres/migrations/V15__Add_min_memory_increase_service.sql
+++ cuebot/src/main/resources/conf/ddl/postgres/migrations/V15__Add_min_memory_increase_service.sql
@@ -2,4 +2,4 @@
 -- Add minimum memory increase - default 2G
 
 ALTER TABLE show_service ADD COLUMN int_min_memory_increase INT DEFAULT 2097152 NOT NULL;
-ALTER TABLE service ADD COLUMN int_min_memory_increase INT DEFAULT 2097152 NOT NULL;
+ALTER TABLE service ADD COLUMN int_min_memory_increase INT DEFAULT 2097153 NOT NULL;
