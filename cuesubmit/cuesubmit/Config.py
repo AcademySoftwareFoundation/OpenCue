1928707c3991396c4fa98fcc56f7b693171049de
Kern Attila GERMAIN <5556461+KernAttila@users.noreply.github.com>
2023-03-14 22:06:13 +0100

    feat!: update getConfigValues(), moved yaml loading to a separate function. load RENDER_CMDS sub `config_file`

--- cuesubmit/cuesubmit/Config.py
+++ cuesubmit/cuesubmit/Config.py
@@ -35,19 +35,47 @@ CONFIG_FILE_ENV_VAR = 'CUESUBMIT_CONFIG_FILE'
 
 def getConfigValues():
     """Reads the config file from disk and returns the values it defines."""
-    configData = {}
     configFile = os.environ.get(CONFIG_FILE_ENV_VAR)
     if not configFile:
         configFile = os.path.join(opencue.config.config_base_directory(), 'cuesubmit.yaml')
-    if os.path.exists(configFile):
-        with open(configFile, 'r') as data:
-            try:
-                configData = yaml.load(data, Loader=yaml.SafeLoader)
-            except yaml.YAMLError:
-                raise CuesubmitConfigError("Could not load yaml file: {}. Please check its "
-                                           "formatting".format(configFile))
+    configData = _load_yaml_file(yaml_file=configFile)
+    if 'RENDER_CMDS' in configData:
+        configData['RENDER_CMDS'] = _expand_render_config_values(configData['RENDER_CMDS'])
     return configData
 
+def _load_yaml_file(yaml_file):
+    """ Load config yaml as dict
+    :param yaml_file: path to a config.yaml file (path can be an env var)
+    :type yaml_file: str
+    :returns: yaml content as dict
+    :rtype: dict
+    """
+    _yaml_file = os.path.expandvars(yaml_file)
+    if not os.path.exists(_yaml_file):
+        raise RuntimeError(f'{yaml_file=} not found')
+    config_data = {}
+    with open(_yaml_file, 'r') as data:
+        try:
+            config_data = yaml.load(data, Loader=yaml.SafeLoader)
+        except yaml.YAMLError:
+            raise CuesubmitConfigError("Could not load yaml file: {}. Please check its "
+                                       "formatting".format(_yaml_file))
+    return config_data
+
+def _expand_render_config_values(RENDER_CMDS):
+    """ Looks through each render command and loads their 'config_file' if any
+    :param RENDER_CMDS: all render commands from the cuesubmit_config.yaml file
+    :type RENDER_CMDS: dict
+    :returns: Updated RENDER_CMDS dict
+    :rtype: dict
+    """
+    for job_type, _options in RENDER_CMDS.items():
+        _sub_config_file = _options.get('config_file')
+        if not _sub_config_file:
+            continue
+        RENDER_CMDS[job_type] = _load_yaml_file(yaml_file=_sub_config_file)
+
+    return RENDER_CMDS
 
 class CuesubmitConfigError(Exception):
     """Thrown when an error occurs reading the config file."""
