d1ba72c2cf8e8940a160621a81ff921d0193d3fa
Kern Attila GERMAIN <5556461+KernAttila@users.noreply.github.com>
2023-03-14 15:32:26 +0100

    feat: use the list of env var from `RQD_HOST_ENV_VARS` to copy them in the frame env.

--- rqd/rqd/rqcore.py
+++ rqd/rqd/rqcore.py
@@ -104,6 +104,9 @@ class FrameAttendantThread(threading.Thread):
             for variable in ["SYSTEMROOT", "APPDATA", "TMP", "COMMONPROGRAMFILES"]:
                 if variable in os.environ:
                     self.frameEnv[variable] = os.environ[variable]
+        for variable in rqd.rqconstants.RQD_HOST_ENV_VARS:
+            # Fallback to empty string, easy to spot what is missing in the log
+            self.frameEnv[variable] = os.environ.get(variable, '')
 
         for key, value in self.runFrame.environment.items():
             if key == 'PATH':
