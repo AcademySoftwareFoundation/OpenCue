f9de3533c1848d4940de440b6a065f2fab50c38f
Kern Attila GERMAIN <5556461+KernAttila@users.noreply.github.com>
2023-03-14 21:45:45 +0100

    feat!: put job specific widgets inside a groupBox for clarity Set the groupBox title for each settingsWidget

--- cuesubmit/cuesubmit/ui/SettingsWidgets.py
+++ cuesubmit/cuesubmit/ui/SettingsWidgets.py
@@ -35,6 +35,11 @@ class BaseSettingsWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(BaseSettingsWidget, self).__init__(parent)
         self.mainLayout = QtWidgets.QVBoxLayout()
+        self.groupBox = QtWidgets.QGroupBox('options')
+        self.groupLayout = QtWidgets.QVBoxLayout()
+        self.groupBox.setLayout(self.groupLayout)
+        self.groupBox.setStyleSheet(Widgets.Style.GROUP_BOX)
+        self.mainLayout.addWidget(self.groupBox)
         self.setLayout(self.mainLayout)
         self.mainLayout.setContentsMargins(0, 0, 0, 0)
 
@@ -53,6 +58,7 @@ class InMayaSettings(BaseSettingsWidget):
     # pylint: disable=keyword-arg-before-vararg,unused-argument
     def __init__(self, cameras=None, filename=None, parent=None, *args, **kwargs):
         super(InMayaSettings, self).__init__(parent=parent)
+        self.groupBox.setTitle('Maya options')
         self.mayaFileInput = Widgets.CueLabelLineEdit('Maya File:', filename)
         self.cameraSelector = Widgets.CueSelectPulldown('Render Cameras', options=cameras)
         self.selectorLayout = QtWidgets.QHBoxLayout()
@@ -60,10 +66,10 @@ class InMayaSettings(BaseSettingsWidget):
 
     def setupUi(self):
         """Creates the Maya-specific widget layout."""
-        self.mainLayout.addWidget(self.mayaFileInput)
+        self.groupLayout.addWidget(self.mayaFileInput)
         self.selectorLayout.addWidget(self.cameraSelector)
         self.selectorLayout.addSpacerItem(Widgets.CueSpacerItem(Widgets.SpacerTypes.HORIZONTAL))
-        self.mainLayout.addLayout(self.selectorLayout)
+        self.groupLayout.addLayout(self.selectorLayout)
 
     def setCommandData(self, commandData):
         self.mayaFileInput.setText(commandData.get('mayaFile', ''))
@@ -88,7 +94,7 @@ class BaseMayaSettings(BaseSettingsWidget):
 
     def setupUi(self):
         """Creates the widget layout with a single input for the path to the Maya scene."""
-        self.mainLayout.addWidget(self.mayaFileInput)
+        self.groupLayout.addWidget(self.mayaFileInput)
 
     def setupConnections(self):
         """Sets up widget signals."""
@@ -109,6 +115,7 @@ class InNukeSettings(BaseSettingsWidget):
     # pylint: disable=keyword-arg-before-vararg,unused-argument
     def __init__(self, writeNodes=None, filename=None, parent=None, *args, **kwargs):
         super(InNukeSettings, self).__init__(parent=parent)
+        self.groupBox.setTitle('Nuke options')
         self.fileInput = Widgets.CueLabelLineEdit('Nuke File:', filename)
         self.writeNodeSelector = Widgets.CueSelectPulldown('Write Nodes:', emptyText='[All]',
                                                            options=writeNodes)
@@ -117,10 +124,10 @@ class InNukeSettings(BaseSettingsWidget):
 
     def setupUi(self):
         """Creates the Nuke-specific widget layout."""
-        self.mainLayout.addWidget(self.fileInput)
+        self.groupLayout.addWidget(self.fileInput)
         self.selectorLayout.addWidget(self.writeNodeSelector)
         self.selectorLayout.addSpacerItem(Widgets.CueSpacerItem(Widgets.SpacerTypes.HORIZONTAL))
-        self.mainLayout.addLayout(self.selectorLayout)
+        self.groupLayout.addLayout(self.selectorLayout)
 
     def setCommandData(self, commandData):
         self.fileInput.setText(commandData.get('nukeFile', ''))
@@ -145,7 +152,7 @@ class BaseNukeSettings(BaseSettingsWidget):
 
     def setupUi(self):
         """Creates the widget layout with a single input for the path to the Nuke script."""
-        self.mainLayout.addWidget(self.fileInput)
+        self.groupLayout.addWidget(self.fileInput)
 
     def setupConnections(self):
         """Sets up widget signals."""
@@ -166,7 +173,7 @@ class ShellSettings(BaseSettingsWidget):
     # pylint: disable=keyword-arg-before-vararg,unused-argument
     def __init__(self, parent=None, *args, **kwargs):
         super(ShellSettings, self).__init__(parent=parent)
-
+        self.groupBox.setTitle('Shell options')
         self.commandTextBox = Command.CueCommandWidget()
 
         self.setupUi()
@@ -174,7 +181,7 @@ class ShellSettings(BaseSettingsWidget):
 
     def setupUi(self):
         """Creates the widget layout with a single input for the shell command."""
-        self.mainLayout.addWidget(self.commandTextBox)
+        self.groupLayout.addWidget(self.commandTextBox)
 
     def setupConnections(self):
         """Sets up widget signals."""
@@ -207,8 +214,8 @@ class BaseBlenderSettings(BaseSettingsWidget):
 
     def setupUi(self):
         """Creates the Blender-specific widget layout."""
-        self.mainLayout.addWidget(self.fileInput)
-        self.mainLayout.addLayout(self.outputLayout)
+        self.groupLayout.addWidget(self.fileInput)
+        self.groupLayout.addLayout(self.outputLayout)
         self.outputLayout.addWidget(self.outputPath)
         self.outputLayout.addWidget(self.outputSelector)
 
