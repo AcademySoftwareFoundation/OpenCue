f5f3091982a36777f541ca4f24b22db612d54097
Kern Attila Germain <5556461+KernAttila@users.noreply.github.com>
2023-03-29 00:53:36 +0200

    fix: test if file exists

--- cuesubmit/cuesubmit/Config.py
+++ cuesubmit/cuesubmit/Config.py
@@ -38,7 +38,7 @@ def getConfigValues():
     configFile = os.environ.get(CONFIG_FILE_ENV_VAR)\
                  or os.path.join(opencue.config.config_base_directory(),
                                  'cuesubmit.yaml')
-    if not configFile:
+    if not os.path.exists(configFile):
         return {}
     configData = _loadYamlFile(yaml_file=configFile)
     if 'RENDER_CMDS' in configData:
