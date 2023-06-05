2081bf4e8f25fddfe151467513cd5ce5b30a0b25
Kern Attila GERMAIN <5556461+kernattila@users.noreply.github.com>
2023-06-05 13:48:39 +0200

    fix: syntax

--- cuebot/src/main/java/com/imageworks/spcue/service/JobSpec.java
+++ cuebot/src/main/java/com/imageworks/spcue/service/JobSpec.java
@@ -673,7 +673,7 @@ public class JobSpec {
         else if (layerTag.getChildTextTrim("threadable") != null) {
             layer.isThreadable = Convert.stringToBool(
                     layerTag.getChildTextTrim("threadable"));
-            logger.debug("layerTag.getChildTextTrim('threadable') : " + layerTag.getChildTextTrim('threadable'));
+            logger.debug("layerTag.getChildTextTrim('threadable') : " + layerTag.getChildTextTrim("threadable"));
         }
         logger.debug("layer.isThreadable : " + layer.isThreadable);
     }
