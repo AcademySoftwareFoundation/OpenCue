2656031b773b74e2b22d1f168b3da27b98ad6345
n-jay <nuwanjayawardene@gmail.com>
2023-06-05 14:12:36 +0530

    Install external dependencies from text file

--- cuesubmit/plugins/OpenCue/Setup.py
+++ cuesubmit/plugins/OpenCue/Setup.py
@@ -35,7 +35,6 @@ def python_exec():
     if isWindows():
         return os.path.join(sys.prefix, 'bin', 'python.exe')
     elif isMacOS():
-
         try:
             # 2.92 and older
             path = bpy.app.binary_path_python
@@ -47,19 +46,22 @@ def python_exec():
         return os.path.join(sys.prefix, 'bin', 'python3.10')  # Works on Blender 3.3.1 LTS
     else:
         print("sorry, still not implemented for ", os.name, " - ", platform.system)
-
         return os.path.join(sys.prefix, 'sys.prefix/bin', 'python')
 
 
-def installModule(packageName):
+def installModule():
+    # Get path of requirements file
+    script_file = os.path.realpath(__file__)
+    directory = os.path.dirname(script_file)
+    file_name = "requirements.txt"
+    requirements = os.path.join(directory, file_name)
+
+    # identify for platform
     python_exe = python_exec()
 
     # upgrade pip
     subprocess.call([python_exe, "-m", "ensurepip"])
     subprocess.call([python_exe, "-m", "pip", "install", "--upgrade", "pip"])
     # install required packages
-    subprocess.call([python_exe, "-m", "pip", "install", packageName])
+    subprocess.call([python_exe, "-m", "pip", "install", "-r", requirements])
 
-
-installModule("future==0.17.1")
-installModule("PyYAML==5.1")
