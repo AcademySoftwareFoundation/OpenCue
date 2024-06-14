6837800002e34a869e2799d99bf523af8c644d22
n-jay <nuwanjayawardene@gmail.com>
2024-06-14 12:55:39 +0530

    Add jq slurp option
    In verify_flyway_success function

--- ci/run_integration_test.sh
+++ ci/run_integration_test.sh
@@ -102,7 +102,7 @@ wait_for_service_state() {
 }
 
 verify_flyway_success() {
-    container=$(docker compose ps --all --format json | jq '.[] | select(.Service=="flyway")')
+    container=$(docker compose ps --all --format json | jq -s '.[] | select(.Service=="flyway")')
     container_name=$(echo "$container" | jq -r '.Name')
     exit_code=$(echo "$container" | jq -r '.ExitCode')
     if [[ ${exit_code} = 0 ]]; then
