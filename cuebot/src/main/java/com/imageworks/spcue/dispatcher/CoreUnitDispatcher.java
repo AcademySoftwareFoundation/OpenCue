034da7970e017f3c9b21ce150aba21b1ac673d97
Diego Tavares <dtavares@imageworks.com>
2024-06-19 16:00:44 -0700

    Feature - Selfish services
    A selfish service makes sure its frames have all the host available cores reserved, ensuring this job will not be competing resources with other frames.
    
    Selfish services can be configured on the opencue.properties file on `dispatcher.frame.selfish.services`.

--- cuebot/src/main/java/com/imageworks/spcue/dispatcher/CoreUnitDispatcher.java
+++ cuebot/src/main/java/com/imageworks/spcue/dispatcher/CoreUnitDispatcher.java
@@ -259,9 +259,10 @@ public class CoreUnitDispatcher implements Dispatcher {
                 host.getName() + " " + host.idleCores + "/" + host.idleMemory +
                 " on job " + job.getName());
 
+        String[] selfishServices = env.getProperty("dispatcher.frame.selfish.services", "").split(",");
         for (DispatchFrame frame: frames) {
 
-            VirtualProc proc =  VirtualProc.build(host, frame);
+            VirtualProc proc =  VirtualProc.build(host, frame, selfishServices);
 
             if (host.idleCores < frame.minCores ||
                     host.idleMemory < frame.minMemory ||
