a5ecbff685abac30396ab3ee7b5ba25011efafa9
Jimmy Christensen <lithorus@gmail.com>
2024-07-01 23:27:41 +0200

    Fix pylint errors

--- rqd/rqd/rqlogging.py
+++ rqd/rqd/rqlogging.py
@@ -1,3 +1,19 @@
+#  Copyright Contributors to the OpenCue Project
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+"""Logging module, handles logging to files and non-files"""
 
 import time
 import os
@@ -6,20 +22,26 @@ import datetime
 
 
 def createLogger(filepath):
+    """Wrapper to create the RQDLogger object"""
     f = RQDLogger(filepath)
     return f
 
 
 class RQDLogger(tempfile.SpooledTemporaryFile):
-
+    """Class to abstract file logging, this class tries to act as a file object"""
     filepath = None
     fd = None
 
     def __init__(self, filepath):
+        """RQDLogger class initialization
+           @type    filepath: string
+           @param   filepath: The filepath to log to
+        """
         super().__init__()
         self.filepath = filepath
         self.fd = open(self.filepath, "w+", 1)
 
+    # pylint: disable=arguments-differ
     def write(self, string, prependTimestamp=False):
         if prependTimestamp is True:
             lines = string.splitlines()
