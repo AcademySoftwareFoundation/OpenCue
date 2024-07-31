b3552b6c8560355a8392c980c19bb27586e7b6f1
Jimmy Christensen <lithorus@gmail.com>
2024-07-31 21:05:14 +0200

    Remove LOGTYPE_FILE and renamed class to RqdLogger

--- rqd/rqd/rqlogging.py
+++ rqd/rqd/rqlogging.py
@@ -25,13 +25,11 @@ import platform
 
 import rqd.rqconstants
 
-LOGTYPE_FILE = 1
-
 log = logging.getLogger(__name__)
 log.setLevel(rqd.rqconstants.CONSOLE_LOG_LEVEL)
 
 
-class RQDLogger(object):
+class RqdLogger(object):
     """Class to abstract file logging, this class tries to act as a file object"""
     filepath = None
     fd = None
@@ -43,67 +41,57 @@ class RQDLogger(object):
            @param   filepath: The filepath to log to
         """
 
-        protocolMatch = re.match(r"(?P<proto>[\w\-.:]+)://(?P<server>\w+)/(?P<filepath>.*)",
-                                 filepath)
-        if protocolMatch is not None:
-            self.filepath = protocolMatch.group('filepath')
-            self.server = protocolMatch.group('server')
-        else:
-            self.type = LOGTYPE_FILE
-            self.filepath = filepath
+        self.filepath = filepath
 
-        if self.type == LOGTYPE_FILE:
-            log_dir = os.path.dirname(self.filepath)
+        log_dir = os.path.dirname(self.filepath)
+        if not os.access(log_dir, os.F_OK):
+            # Attempting mkdir for missing logdir
+            msg = "No Error"
+            try:
+                os.makedirs(log_dir)
+                os.chmod(log_dir, 0o777)
+            # pylint: disable=broad-except
+            except Exception as e:
+                # This is expected to fail when called in abq
+                # But the directory should now be visible
+                msg = e
+
             if not os.access(log_dir, os.F_OK):
-                # Attempting mkdir for missing logdir
-                msg = "No Error"
-                try:
-                    os.makedirs(log_dir)
-                    os.chmod(log_dir, 0o777)
-                # pylint: disable=broad-except
-                except Exception as e:
-                    # This is expected to fail when called in abq
-                    # But the directory should now be visible
-                    msg = e
-
-                if not os.access(log_dir, os.F_OK):
-                    err = "Unable to see log directory: %s, mkdir failed with: %s" % (
-                        log_dir, msg)
-                    raise RuntimeError(err)
-
-            if not os.access(log_dir, os.W_OK):
-                err = "Unable to write to log directory %s" % log_dir
+                err = "Unable to see log directory: %s, mkdir failed with: %s" % (
+                    log_dir, msg)
                 raise RuntimeError(err)
 
-            try:
-                # Rotate any old logs to a max of MAX_LOG_FILES:
-                if os.path.isfile(self.filepath):
-                    rotateCount = 1
-                    while (os.path.isfile("%s.%s" % (self.filepath, rotateCount))
-                           and rotateCount < rqd.rqconstants.MAX_LOG_FILES):
-                        rotateCount += 1
-                    os.rename(self.filepath,
-                              "%s.%s" % (self.filepath, rotateCount))
-            # pylint: disable=broad-except
-            except Exception as e:
-                err = "Unable to rotate previous log file due to %s" % e
-                # Windows might fail while trying to rotate logs for checking if file is
-                # being used by another process. Frame execution doesn't need to
-                # be halted for this.
-                if platform.system() == "Windows":
-                    log.warning(err)
-                else:
-                    raise RuntimeError(err)
-            # pylint: disable=consider-using-with
-            self.fd = open(self.filepath, "w+", 1, encoding='utf-8')
-            try:
-                os.chmod(self.filepath, 0o666)
-            # pylint: disable=broad-except
-            except Exception as e:
-                err = "Failed to chmod log file! %s due to %s" % (self.filepath, e)
+        if not os.access(log_dir, os.W_OK):
+            err = "Unable to write to log directory %s" % log_dir
+            raise RuntimeError(err)
+
+        try:
+            # Rotate any old logs to a max of MAX_LOG_FILES:
+            if os.path.isfile(self.filepath):
+                rotateCount = 1
+                while (os.path.isfile("%s.%s" % (self.filepath, rotateCount))
+                       and rotateCount < rqd.rqconstants.MAX_LOG_FILES):
+                    rotateCount += 1
+                os.rename(self.filepath,
+                          "%s.%s" % (self.filepath, rotateCount))
+        # pylint: disable=broad-except
+        except Exception as e:
+            err = "Unable to rotate previous log file due to %s" % e
+            # Windows might fail while trying to rotate logs for checking if file is
+            # being used by another process. Frame execution doesn't need to
+            # be halted for this.
+            if platform.system() == "Windows":
                 log.warning(err)
-        else:
-            raise Exception("Unknown file path type")
+            else:
+                raise RuntimeError(err)
+        # pylint: disable=consider-using-with
+        self.fd = open(self.filepath, "w+", 1, encoding='utf-8')
+        try:
+            os.chmod(self.filepath, 0o666)
+        # pylint: disable=broad-except
+        except Exception as e:
+            err = "Failed to chmod log file! %s due to %s" % (self.filepath, e)
+            log.warning(err)
 
     # pylint: disable=arguments-differ
     def write(self, data, prependTimestamp=False):
@@ -117,8 +105,7 @@ class RQDLogger(object):
             for line in lines:
                 print("[%s] %s" % (curr_line_timestamp, line), file=self)
         else:
-            if self.type == LOGTYPE_FILE:
-                self.fd.write(data)
+            self.fd.write(data)
 
     def writelines(self, __lines):
         """Provides support for writing mutliple lines at a time"""
@@ -127,20 +114,18 @@ class RQDLogger(object):
 
     def close(self):
         """Closes the file if the backend is file based"""
-        if self.type == LOGTYPE_FILE:
-            self.fd.close()
+        self.fd.close()
 
     def waitForFile(self, maxTries=5):
         """Waits for the file to exist before continuing when using a file backend"""
-        if self.type == LOGTYPE_FILE:
-            # Waits for a file to exist
-            tries = 0
-            while tries < maxTries:
-                if os.path.exists(self.filepath):
-                    return
-                tries += 1
-                time.sleep(0.5 * tries)
-            raise IOError("Failed to create %s" % self.filepath)
+        # Waits for a file to exist
+        tries = 0
+        while tries < maxTries:
+            if os.path.exists(self.filepath):
+                return
+            tries += 1
+            time.sleep(0.5 * tries)
+        raise IOError("Failed to create %s" % self.filepath)
 
     def __enter__(self):
         return self
