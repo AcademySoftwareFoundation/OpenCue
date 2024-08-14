0e7b590892f0193ecb54454de0489f37c5566ae0
abrand <anton.brand@ubisoft.com>
2024-08-14 13:35:05 -0400

    add wmi requirement

--- rqd/setup.py
+++ rqd/setup.py
@@ -61,7 +61,8 @@ setup(
         'grpcio',
         'grpcio-tools',
         'psutil',
-        'pywin32; platform_system == "Windows"'
+        'pywin32; platform_system == "Windows"',
+        'wmi==1.5.1; platform_system == "Windows"'
     ]
 )
 
