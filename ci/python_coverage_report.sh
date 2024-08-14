d87dacb609010a968d888da919db91e83e8cb4b5
Jimmy Christensen <lithorus@gmail.com>
2024-08-14 22:55:11 +0200

    Remove rqd/compiled_proto from tests and documentation

--- ci/python_coverage_report.sh
+++ ci/python_coverage_report.sh
@@ -6,9 +6,7 @@ pip install --user -r requirements.txt -r requirements_gui.txt
 
 # Protos need to have their Python code generated in order for tests to pass.
 python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
-python -m grpc_tools.protoc -I=proto/ --python_out=rqd/rqd/compiled_proto --grpc_python_out=rqd/rqd/compiled_proto proto/*.proto
 2to3 -wn -f import pycue/opencue/compiled_proto/*_pb2*.py
-2to3 -wn -f import rqd/rqd/compiled_proto/*_pb2*.py
 
 # Run coverage for each component individually, but append it all into the same report.
 coverage run --source=pycue/opencue/,pycue/FileSequence/ --omit=pycue/opencue/compiled_proto/* pycue/setup.py test
@@ -16,7 +14,7 @@ PYTHONPATH=pycue coverage run -a --source=pyoutline/outline/ pyoutline/setup.py 
 PYTHONPATH=pycue coverage run -a --source=cueadmin/cueadmin/ cueadmin/setup.py test
 PYTHONPATH=pycue xvfb-run -d coverage run -a --source=cuegui/cuegui/ cuegui/setup.py test
 PYTHONPATH=pycue:pyoutline coverage run -a --source=cuesubmit/cuesubmit/ cuesubmit/setup.py test
-coverage run -a --source=rqd/rqd/ --omit=rqd/rqd/compiled_proto/* rqd/setup.py test
+coverage run -a --source=rqd/rqd/ --omit=pycue/opencue/compiled_proto/* rqd/setup.py test
 
 # SonarCloud needs the report in XML.
 coverage xml
--- ci/run_python_tests_pyside6.sh
+++ ci/run_python_tests_pyside6.sh
@@ -33,11 +33,9 @@ python3 -m pip install --user PySide6==6.3.2
 
 # Protos need to have their Python code generated in order for tests to pass.
 python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
-python -m grpc_tools.protoc -I=proto/ --python_out=rqd/rqd/compiled_proto --grpc_python_out=rqd/rqd/compiled_proto proto/*.proto
 
 # Fix compiled proto code for Python 3.
 2to3 -wn -f import pycue/opencue/compiled_proto/*_pb2*.py
-2to3 -wn -f import rqd/rqd/compiled_proto/*_pb2*.py
 
 python pycue/setup.py test
 PYTHONPATH=pycue python pyoutline/setup.py test
