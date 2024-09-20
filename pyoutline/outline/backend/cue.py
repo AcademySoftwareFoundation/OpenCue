007650093638ca7ef18e60e619dd6a0717c197ec
Kern Attila GERMAIN <5556461+KernAttila@users.noreply.github.com>
2024-09-21 01:06:33 +0200

    refactor: only override job minCores if it is explicitly overridden

--- pyoutline/outline/backend/cue.py
+++ pyoutline/outline/backend/cue.py
@@ -319,12 +319,15 @@ def _serialize(launcher, use_pycuerun):
 
         # opencue specific options
         # Keeping 'threads' for backward compatibility
-        if layer.get_arg("cores"):
-            if layer.get_arg("threads"):
+        cores = None
+        if layer.get_arg("overrideCores"):
+            logger.info("%s is set to override service cores.", layer.get_name())
+            if layer.is_arg_set("cores") and layer.is_arg_set("threads"):
                 logger.warning("%s has both cores and threads. Use cores.", layer.get_name())
-            sub_element(spec_layer, "cores", "%0.1f" % (layer.get_arg("cores")))
-        elif layer.get_arg("threads"):
-            sub_element(spec_layer, "cores", "%0.1f" % (layer.get_arg("threads")))
+            cores = "%0.1f" % float(layer.get_arg("cores") or layer.get_arg("threads", 0))
+            sub_element(spec_layer, "cores", cores)
+        else:
+            logger.warning("%s will use service cores.", layer.get_name())
 
         if layer.is_arg_set("threadable"):
             sub_element(spec_layer, "threadable",
