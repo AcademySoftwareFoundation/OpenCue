b98758a507888a7ac4ca5093a5eacad5a3767540
Kern Attila GERMAIN <5556461+KernAttila@users.noreply.github.com>
2024-09-29 18:22:12 +0200

    fix: use platform.uname() instead of os.uname(). They have the same result on Linux, but os.uname() is not available on Windows, making us unable to kill a job from this OS.

--- pycue/opencue/wrappers/job.py
+++ pycue/opencue/wrappers/job.py
@@ -17,6 +17,7 @@
 import enum
 import getpass
 import os
+import platform
 import time
 
 from opencue import Cuebot
@@ -49,7 +50,7 @@ class Job(object):
         """Kills the job."""
         username = username if username else getpass.getuser()
         pid = pid if pid else os.getpid()
-        host_kill = host_kill if host_kill else os.uname()[1]
+        host_kill = host_kill if host_kill else platform.uname()[1]
         self.stub.Kill(job_pb2.JobKillRequest(job=self.data,
                                               username=username,
                                               pid=str(pid),
@@ -73,7 +74,7 @@ class Job(object):
         """
         username = username if username else getpass.getuser()
         pid = pid if pid else os.getpid()
-        host_kill = host_kill if host_kill else os.uname()[1]
+        host_kill = host_kill if host_kill else platform.uname()[1]
         criteria = opencue.search.FrameSearch.criteriaFromOptions(**request)
         self.stub.KillFrames(job_pb2.JobKillFramesRequest(job=self.data,
                                                           req=criteria,
