fd681d6966048b9f03283c21e6b29e4302d5e157
Diego Tavares <dtavares@imageworks.com>
2024-10-02 10:31:37 -0700

    Another attempt on fixing the packaging pipeline
    Some packages require pycue to run their unit tests and this requirement wasn't being installed on the dockerfile.

---
+++ cueadmin/tests/test_suite.py
@@ -0,0 +1,12 @@
+import unittest
+
+def create_test_suite():
+    loader = unittest.TestLoader()
+    start_dir = '.'  # Specify the directory where your test files reside
+    suite = loader.discover(start_dir, pattern='*_tests.py')
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    test_suite = create_test_suite()
+    runner.run(test_suite)
