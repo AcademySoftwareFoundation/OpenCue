9db2eb4488747a7a9f4f834b8bde082b52ab27c5
Jimmy Christensen <lithorus@gmail.com>
2024-10-18 23:12:47 +0200

    Add tests for rqd using vanilla python and introduce requirements dir with split up requirements

---
+++ ci/run_rqd_python_tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script for running OpenCue RQD unit tests with vanilla python
+#
+# This script is written to be run within the OpenCue GitHub Actions environment.
+# See `.github/workflows/testing-pipeline.yml`.
+
+set -e
+
+args=("$@")
+python_version=$(python -V 2>&1)
+echo "Will run tests using ${python_version}"
+
+pip install --user -r requirements/rqd.txt -r requirements/tests.txt
+
+# Protos need to have their Python code generated in order for tests to pass.
+python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
+python -m grpc_tools.protoc -I=proto/ --python_out=rqd/rqd/compiled_proto --grpc_python_out=rqd/rqd/compiled_proto proto/*.proto
+
+# Fix imports to work in both Python 2 and 3. See
+# <https://github.com/protocolbuffers/protobuf/issues/1491> for more info.
+2to3 -wn -f import pycue/opencue/compiled_proto/*_pb2*.py
+2to3 -wn -f import rqd/rqd/compiled_proto/*_pb2*.py
+
+python3 -m unittest discover -s pycue/tests -t pycue -p "*.py"
+python3 -m unittest discover -s rqd/tests -t rqd -p "*.py"
+
