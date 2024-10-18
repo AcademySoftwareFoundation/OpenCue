a2283f4dfa99c182283f91848d26dc9f82795f50
Diego Tavares <dtavares@imageworks.com>
2024-10-18 09:35:45 -0700

    Fix logging for deleteFrame
    Logging was added on the wrong scope, which led to a "Frame not found in cache" when a frame was actually found.

--- rqd/rqd/rqcore.py
+++ rqd/rqd/rqcore.py
@@ -222,9 +222,9 @@ class RqCore(object):
                         self.cores.reserved_cores)
                     # pylint: disable=no-member
                     self.cores.reserved_cores.clear()
-                    log.info("Successfully delete frame with Id: %s", frameId)
-                else:
-                    log.warning("Frame with Id: %s not found in cache", frameId)
+                log.info("Successfully delete frame with Id: %s", frameId)
+            else:
+                log.warning("Frame with Id: %s not found in cache", frameId)
 
     def killAllFrame(self, reason):
         """Will execute .kill() on every frame in cache until no frames remain
