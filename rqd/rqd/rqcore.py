AcademySoftwareFoundation/OpenCue

commit f7244ddd0da060b04617d5cc6cab5fa3f9a0d07b

[rqd] Avoid changing dict in place during iteration (#1554)
Deleting an item from the dict being iterated over on sanitizeFrames
caused the error: "Dictionary changed size during iteration".

rqd/rqd/rqcore.py
@@ -1091,7 +1091,8 @@ class RqCore(object):
         Iterate over the cache and update the status of frames that might have
         completed but never reported back to cuebot.
         """
-        for frameId, runningFrame in self.__cache.items():
+        for frameId in list(self.__cache.keys):
+            runningFrame = self.__cache[frameId]
             # If the frame was marked as completed (exitStatus) and a report has not been sent
             # try to file the report again
             if runningFrame.exitStatus is not None and not runningFrame.completeReportSent:

commit 5d39b2649814eb3938651b7ce7567f75dd210410

[rqd] Fix `keys` not iterable since it's a built-in function. (#1564)
**Link the Issue(s) this Pull Request is related to.**
This fixes an bug introduced with #1554 

**Summarize your change.**
It fixes a small bug that hinders `rqd` to actually run any jobs. It
probably didn't get chaught due to the unittests for rqd not working
properly.

rqd/rqd/rqcore.py
@@ -1091,7 +1091,7 @@ class RqCore(object):
         Iterate over the cache and update the status of frames that might have
         completed but never reported back to cuebot.
         """
-        for frameId in list(self.__cache.keys):
+        for frameId in list(self.__cache.keys()):
             runningFrame = self.__cache[frameId]
             # If the frame was marked as completed (exitStatus) and a report has not been sent
             # try to file the report again