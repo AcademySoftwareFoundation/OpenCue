6094a1540a27f31955a3ec96731b7058552c77bf
Diego Tavares <dtavares@imageworks.com>
2024-11-18 10:10:17 -0800

    Handle minimum limit requirement

--- rqd/rqd/rqcore.py
+++ rqd/rqd/rqcore.py
@@ -1039,6 +1039,20 @@ exec su -s %s %s -c "echo \$$; /bin/nice /usr/bin/time -p -o %s %s %s"
             command.replace(tempPassword, "[password]").replace(";", "\n"),
             prependTimestamp=rqd.rqconstants.RQD_PREPEND_TIMESTAMP)
 
+        # Handle memory limits
+        # Docker min requirement is 6MB, if request is bellow limit,
+        # give the frame a reasonable amount of memory
+        soft_memory_limit = runFrame.soft_memory_limit
+        if soft_memory_limit <= 6291456:
+            logging.warning("Frame requested %s bytes of soft_memory_limit, which is lower than "
+                            "minimum required. Running with 1MB", soft_memory_limit)
+            soft_memory_limit = "1GB"
+        hard_memory_limit = runFrame.soft_memory_limit
+        if hard_memory_limit <= 6291456:
+            logging.warning("Frame requested %s bytes of hard_memory_limit, which is lower than "
+                            "minimum required. Running with 2MB", hard_memory_limit)
+            hard_memory_limit = "2GB"
+
         # Write command to a file on the job tmpdir to simplify replaying a frame
         command = self._createCommandFile(command)
         docker_client = self.rqCore.docker.from_env()
@@ -1058,8 +1072,8 @@ exec su -s %s %s -c "echo \$$; /bin/nice /usr/bin/time -p -o %s %s %s"
                     network="host",
                     stderr=True,
                     hostname=self.frameEnv["jobhost"],
-                    mem_reservation=runFrame.soft_memory_limit,
-                    mem_limit=runFrame.hard_memory_limit,
+                    mem_reservation=soft_memory_limit,
+                    mem_limit=hard_memory_limit,
                     entrypoint=command)
 
             log_stream = container.logs(stream=True)
