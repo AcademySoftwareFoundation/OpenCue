AcademySoftwareFoundation/OpenCue

commit 291b6947f82f9fb436fb17fdec512d0abaff6f10

[cuebot/rqd] Add feature to run frames on a containerized environment using docker (#1549)
### Motivation

Running OpenCue In a multi operational system environment requires
segregating the farm, which means hosts have to be assigned to one OS
and cannot be shared between shows that have different OS requirements.
This can be a challenge when sharing resources between shows is
necessary.

### Proposed solution

A new execution mode on **rqd** `runDocker` to live alongside
`runLinux`, `runWindows`, and `runDarwin` (macOs). This mode will launch
the frame command on a docker container based on the frame expected OS.
With this, rqd is now able to run jobs from different OSs on the same
host.

But to make this possible, a rqd host needs to advertise itself not with
its own OS code (defined by `SP_OS` on rqd.conf), but with all the OSs
of images it is capable of executing.

### Configuration changes
The following sections were added to rqd.conf:

```ini
[docker.config]
# Setting this to True requires all the additional "docker.[]" sections to be filled
RUN_ON_DOCKER=True

# This section is only required if RUN_ON_DOCKER=True
# List of volume mounts following docker run's format, but replacing = with :
[docker.mounts]
TEMP=type:bind,source:/tmp,target:/tmp,bind-propagation:slave
NET=type:bind,source:/net,target:/net,bind-propagation:slave

# This section is only required if RUN_ON_DOCKER=True
#  - keys represent OSs this rqd is capable of executing jobs in
#  - values are docker image tags
[docker.images]
centos7=centos7.3:latest
rocky9=rocky9.3:latest
```

In this case, the rqd host would advertise itself with
`OS=centos7,rocky9`, and the dispatch logic has been changed accordingly
to account for dispatching frames to nodes that support multiple OSs.

Feature has been documented at https://github.com/AcademySoftwareFoundation/opencue.io/pull/302
---------

Signed-off-by: Diego Tavares <dtavares@imageworks.com>

cuebot/src/main/java/com/imageworks/spcue/dao/postgres/DispatchQuery.java
@@ -72,9 +72,9 @@ public class DispatchQuery {
                 "AND job.pk_facility                = ? " +
                 "AND " +
                     "(" +
-                        "job.str_os IS NULL OR job.str_os = '' " +
+                        "job.str_os IS NULL OR job.str_os IN '' " +
                     "OR " +
-                        "job.str_os = ? " +
+                        "job.str_os IN ? " +
                     ") " +
                 "AND (CASE WHEN layer_stat.int_waiting_count > 0 THEN 1 ELSE NULL END) = 1 " +
                 "AND layer.int_cores_min            <= ? " +
@@ -135,7 +135,7 @@ public class DispatchQuery {
                     "(" +
                         "job.str_os IS NULL OR job.str_os = '' " +
                     "OR " +
-                        "job.str_os = ? " +
+                        "job.str_os IN ? " +
                     ") " +
                 "AND (CASE WHEN layer_stat.int_waiting_count > 0 THEN 1 ELSE NULL END) = 1 " +
                 "AND layer.int_cores_min            <= ? " +
@@ -250,7 +250,7 @@ public class DispatchQuery {
             "AND " +
                 "job.pk_facility =  ? " +
             "AND " +
-                "(job.str_os = ? OR job.str_os IS NULL) " +
+                "(job.str_os IN ? OR job.str_os IS NULL) " +
             "AND " +
                 "job.pk_job IN ( " +
                     "SELECT " +
@@ -276,7 +276,7 @@ public class DispatchQuery {
                     "AND " +
                         "j.pk_facility = ? " +
                     "AND " +
-                        "(j.str_os = ? OR j.str_os IS NULL) " +
+                        "(j.str_os IN ? OR j.str_os IS NULL) " +
                     "AND " +
                         "(CASE WHEN lst.int_waiting_count > 0 THEN lst.pk_layer ELSE NULL END) = l.pk_layer " +
                     "AND " +
@@ -519,40 +519,42 @@ public class DispatchQuery {
                 ") " +
             "LIMIT 1";
 
+    private static final String FIND_DISPATCH_FRAME_COLUMNS =
+        "show_name, " +
+        "job_name, " +
+        "pk_job, " +
+        "pk_show, " +
+        "pk_facility, " +
+        "str_name, " +
+        "str_shot, " +
+        "str_user, " +
+        "int_uid, " +
+        "str_log_dir, " +
+        "str_os, " +
+        "frame_name, " +
+        "frame_state, " +
+        "pk_frame, " +
+        "pk_layer, " +
+        "int_retries, " +
+        "int_version, " +
+        "layer_name, " +
+        "layer_type, " +
+        "b_threadable, " +
+        "int_cores_min, " +
+        "int_cores_max, " +
+        "int_mem_min, " +
+        "int_gpus_min, " +
+        "int_gpus_max, " +
+        "int_gpu_mem_min, " +
+        "str_cmd, " +
+        "str_range, " +
+        "int_chunk_size, " +
+        "str_services ";
     /**
      * Finds the next frame in a job for a proc.
      */
     public static final String FIND_DISPATCH_FRAME_BY_JOB_AND_PROC =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "b_threadable, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "int_mem_min, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM ( " +
             "SELECT " +
                 "ROW_NUMBER() OVER ( ORDER BY " +
@@ -569,6 +571,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +
@@ -638,36 +641,7 @@ public class DispatchQuery {
      * Find the next frame in a job for a host.
      */
     public static final String FIND_DISPATCH_FRAME_BY_JOB_AND_HOST =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "b_threadable, " +
-            "int_mem_min, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM ( " +
             "SELECT " +
                 "ROW_NUMBER() OVER ( ORDER BY " +
@@ -684,6 +658,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +
@@ -754,36 +729,7 @@ public class DispatchQuery {
 
 
     public static final String FIND_LOCAL_DISPATCH_FRAME_BY_JOB_AND_PROC =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "b_threadable, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "int_mem_min, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM ( " +
             "SELECT " +
                 "ROW_NUMBER() OVER ( ORDER BY " +
@@ -800,6 +746,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +
@@ -863,36 +810,7 @@ public class DispatchQuery {
      * Find the next frame in a job for a host.
      */
     public static final String FIND_LOCAL_DISPATCH_FRAME_BY_JOB_AND_HOST =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "b_threadable, " +
-            "int_mem_min, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM (" +
             "SELECT " +
                 "ROW_NUMBER() OVER ( ORDER BY " +
@@ -909,6 +827,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +
@@ -975,36 +894,7 @@ public class DispatchQuery {
      * Finds the next frame in a job for a proc.
      */
     public static final String FIND_DISPATCH_FRAME_BY_LAYER_AND_PROC =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "b_threadable, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "int_mem_min, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM (" +
             "SELECT " +
                 "ROW_NUMBER() OVER ( ORDER BY " +
@@ -1021,6 +911,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +
@@ -1090,36 +981,7 @@ public class DispatchQuery {
      * Find the next frame in a job for a host.
      */
     public static final String FIND_DISPATCH_FRAME_BY_LAYER_AND_HOST =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "b_threadable, " +
-            "int_mem_min, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM (" +
             "SELECT " +
                 "ROW_NUMBER() OVER ( ORDER BY " +
@@ -1136,6 +998,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +
@@ -1206,36 +1069,7 @@ public class DispatchQuery {
 
 
     public static final String FIND_LOCAL_DISPATCH_FRAME_BY_LAYER_AND_PROC =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "b_threadable, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "int_mem_min, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM (" +
             "SELECT " +
                 "ROW_NUMBER() OVER ( ORDER BY " +
@@ -1252,6 +1086,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +
@@ -1315,36 +1150,7 @@ public class DispatchQuery {
      * Find the next frame in a job for a host.
      */
     public static final String FIND_LOCAL_DISPATCH_FRAME_BY_LAYER_AND_HOST =
-        "SELECT " +
-            "show_name, " +
-            "job_name, " +
-            "pk_job, " +
-            "pk_show, " +
-            "pk_facility, " +
-            "str_name, " +
-            "str_shot, " +
-            "str_user, " +
-            "int_uid, " +
-            "str_log_dir, " +
-            "frame_name, " +
-            "frame_state, " +
-            "pk_frame, " +
-            "pk_layer, " +
-            "int_retries, " +
-            "int_version, " +
-            "layer_name, " +
-            "layer_type, " +
-            "int_cores_min, " +
-            "int_cores_max, " +
-            "b_threadable, " +
-            "int_mem_min, " +
-            "int_gpus_min, " +
-            "int_gpus_max, " +
-            "int_gpu_mem_min, " +
-            "str_cmd, " +
-            "str_range, " +
-            "int_chunk_size, " +
-            "str_services " +
+        "SELECT " + FIND_DISPATCH_FRAME_COLUMNS +
         "FROM (" +
             "SELECT " +
                 "ROW_NUMBER() OVER (ORDER BY " +
@@ -1361,6 +1167,7 @@ public class DispatchQuery {
                 "job.str_user, " +
                 "job.int_uid, " +
                 "job.str_log_dir, " +
+                "job.str_os, " +
                 "frame.str_name AS frame_name, " +
                 "frame.str_state AS frame_state, " +
                 "frame.pk_frame, " +

.github/workflows/testing-pipeline.yml
@@ -7,17 +7,6 @@ on:
     branches: [ master ]
 
 jobs:
-  test_python_2022:
-    name: Run Python Unit Tests (CY2022)
-    runs-on: ubuntu-22.04
-    container: aswf/ci-opencue:2022
-    env:
-      ACTIONS_ALLOW_USE_UNSECURE_NODE_VERSION: true
-    steps:
-      - uses: actions/checkout@v3
-      - name: Run Python Tests
-        run: ci/run_python_tests.sh --no-gui
-
   test_cuebot_2022:
     name: Build Cuebot and Run Unit Tests (CY2022)
     runs-on: ubuntu-22.04

VERSION.in
@@ -1,1 +1,1 @@
-1.0
+1.1

ci/run_python_tests.sh
@@ -28,7 +28,6 @@ PYTHONPATH=pycue python -m unittest discover -s cueadmin/tests -t cueadmin -p "*
 PYTHONPATH=pycue:pyoutline python -m unittest discover -s cuesubmit/tests -t cuesubmit -p "*.py"
 python -m pytest rqd/tests
 
-
 # Xvfb no longer supports Python 2.
 if [[ "$python_version" =~ "Python 3" && ${args[0]} != "--no-gui" ]]; then
   ci/run_gui_test.sh

cuebot/src/main/java/com/imageworks/spcue/DispatchFrame.java
@@ -49,5 +49,8 @@ public class DispatchFrame extends FrameEntity implements FrameInterface {
 
     // A comma separated list of services
     public String services;
+
+    // The Operational System this frame is expected to run in
+    public String os;
 }
 

cuebot/src/main/java/com/imageworks/spcue/DispatchHost.java
@@ -51,7 +51,7 @@ public class DispatchHost extends Entity
     public long gpuMemory;
     public long idleGpuMemory;
     public String tags;
-    public String os;
+    private String os;
 
     public boolean isNimby;
     public boolean isLocalDispatch = false;
@@ -79,6 +79,14 @@ public class DispatchHost extends Entity
 
     public String getFacilityId() {
         return facilityId;
+    }
+
+    public String[] getOs() {
+        return this.os.split(",");
+    }
+
+    public void setOs(String os) {
+        this.os = os;
     }
 
     public boolean canHandleNegativeCoresRequest(int requestedCores) {

cuebot/src/main/java/com/imageworks/spcue/VirtualProc.java
@@ -85,7 +85,9 @@ public class VirtualProc extends FrameEntity implements ProcInterface {
      * @param frame
      * @return
      */
-    public static final VirtualProc build(DispatchHost host, DispatchFrame frame, String... selfishServices) {
+    public static final VirtualProc build(DispatchHost host,
+                                          DispatchFrame frame,
+                                          String... selfishServices) {
         VirtualProc proc = new VirtualProc();
         proc.allocationId = host.getAllocationId();
         proc.hostId = host.getHostId();
@@ -94,7 +96,7 @@ public class VirtualProc extends FrameEntity implements ProcInterface {
         proc.jobId = frame.getJobId();
         proc.showId = frame.getShowId();
         proc.facilityId = frame.getFacilityId();
-        proc.os = host.os;
+        proc.os = frame.os;
 
         proc.hostName = host.getName();
         proc.unbooked = false;
@@ -238,7 +240,7 @@ public class VirtualProc extends FrameEntity implements ProcInterface {
         proc.jobId = frame.getJobId();
         proc.showId = frame.getShowId();
         proc.facilityId = frame.getFacilityId();
-        proc.os = host.os;
+        proc.os = frame.os;
 
         proc.hostName = host.getName();
         proc.unbooked = false;

commit fd2b881ecbe79b404ac11dc38d8222bb8956cc45

[cuebot] Minor fix on dispatch query (#1590)
Fix error introduced by
https://github.com/AcademySoftwareFoundation/OpenCue/commit/291b6947f82f9fb436fb17fdec512d0abaff6f10

cuebot/src/main/java/com/imageworks/spcue/dao/postgres/DispatchQuery.java
@@ -72,7 +72,7 @@ public class DispatchQuery {
                 "AND job.pk_facility                = ? " +
                 "AND " +
                     "(" +
-                        "job.str_os IS NULL OR job.str_os IN '' " +
+                        "job.str_os IS NULL OR job.str_os = '' " +
                     "OR " +
                         "job.str_os IN ? " +
                     ") " +