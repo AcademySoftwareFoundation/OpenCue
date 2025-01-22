bfd382bfc4b66ad8e65c7f25b99fc95dcba47228
Ramon Figueiredo <rfigueiredo@imageworks.com>
2025-01-21 17:13:02 -0800

    Update version file paths to use VERSION.in - Updated version file paths in setup scripts to use VERSION.in (correct file name in the Opencue project) instead of VERSION - Fixes the error when running `sandbox/install-client-sources.sh` - `sandbox/install-client-sources.sh` calls `pip install pycue/ pyoutline/ cueadmin/ cuesubmit/ cuegui/` and the `setup.py` in these directories should use `VERSION.in` not `VERSION`

--- cueadmin/setup.py
+++ cueadmin/setup.py
@@ -20,8 +20,8 @@ cueadmin_dir = os.path.abspath(os.path.dirname(__file__))
 
 version = 'unknown'
 possible_version_paths = [
-    os.path.join(cueadmin_dir, 'VERSION'),
-    os.path.join(os.path.dirname(cueadmin_dir), 'VERSION'),
+    os.path.join(cueadmin_dir, 'VERSION.in'),
+    os.path.join(os.path.dirname(cueadmin_dir), 'VERSION.in'),
 ]
 for possible_version_path in possible_version_paths:
     if os.path.exists(possible_version_path):
