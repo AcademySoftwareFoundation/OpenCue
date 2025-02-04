ec4e9de8f79221ce49e6b9c4c65ba3f90ca487a5
Jimmy Christensen <lithorus@gmail.com>
2025-02-04 23:44:03 +0100

    Disable single check for testing..

--- ci/run_integration_test.sh
+++ ci/run_integration_test.sh
@@ -244,8 +244,8 @@ main() {
     create_and_activate_venv
     log INFO "Installing OpenCue Python libraries..."
     sandbox/install-client-sources.sh
-    log INFO "Testing pycue library..."£
-    test_pycue
+    ## log INFO "Testing pycue library..."
+    ## test_pycue
     log INFO "Testing cueadmin..."
     test_cueadmin
 
