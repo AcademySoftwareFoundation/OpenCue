e63df884a79d4544b486fa62b9e23e077905465a
Jimmy Christensen <lithorus@gmail.com>
2025-02-13 00:38:39 +0100

    Add loki-urllib3-client to rqd tests

--- ci/run_python_tests.sh
+++ ci/run_python_tests.sh
@@ -16,6 +16,9 @@ pip install --user -r requirements.txt -r requirements_gui.txt
 # Some rqd unit tests require docker api
 pip install docker==7.1.0
 
+# Some rqd unit tests require lokiclient
+pip install loki-urllib3-client
+
 # Protos need to have their Python code generated in order for tests to pass.
 python -m grpc_tools.protoc -I=proto/ --python_out=pycue/opencue/compiled_proto --grpc_python_out=pycue/opencue/compiled_proto proto/*.proto
 python -m grpc_tools.protoc -I=proto/ --python_out=rqd/rqd/compiled_proto --grpc_python_out=rqd/rqd/compiled_proto proto/*.proto
