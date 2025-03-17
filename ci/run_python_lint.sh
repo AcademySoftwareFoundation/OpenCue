08f746c6b3c5478eff6e3af795256ef48f09e498
Jimmy Christensen <lithorus@gmail.com>
2025-03-17 22:34:19 +0100

    Install packages as user for pylint script

--- ci/run_python_lint.sh
+++ ci/run_python_lint.sh
@@ -8,10 +8,10 @@ echo "Will run Python lint using ${python_version}"
 python -m pip install pylint==2.15.10 --user
 python -m pylint --version
 
-pip install -e ./cuebot
+pip install ./cuebot --user
 
 echo "Running lint for pycue/..."
-pip install -e ./pycue
+pip install ./pycue[test] --user
 cd pycue
 python -m pylint --rcfile=../ci/pylintrc_main FileSequence
 python -m pylint --rcfile=../ci/pylintrc_main opencue
@@ -19,35 +19,35 @@ python -m pylint --rcfile=../ci/pylintrc_test tests
 cd ..
 
 echo "Running lint for pyoutline/..."
-pip install ./pyoutline
+pip install ./pyoutline[test] --user
 cd pyoutline
 python -m pylint --rcfile=../ci/pylintrc_main outline
 python -m pylint --rcfile=../ci/pylintrc_test tests
 cd ..
 
 echo "Running lint for cueadmin/..."
-pip install ./cueadmin
+pip install ./cueadmin[test] --user
 cd cueadmin
 python -m pylint --rcfile=../ci/pylintrc_main cueadmin
 python -m pylint --rcfile=../ci/pylintrc_test tests
 cd ..
 
 echo "Running lint for cuegui/..."
-pip install ./cuegui
+pip install ./cuegui[test] --user
 cd cuegui
 python -m pylint --rcfile=../ci/pylintrc_main cuegui --ignore=cuegui/images,cuegui/images/crystal --disable=no-member
 python -m pylint --rcfile=../ci/pylintrc_test tests --disable=no-member
 cd ..
 
 echo "Running lint for cuesubmit/..."
-pip install ./cuesubmit
+pip install ./cuesubmit[test] --user
 cd cuesubmit
 python -m pylint --rcfile=../ci/pylintrc_main cuesubmit --disable=no-member
 python -m pylint --rcfile=../ci/pylintrc_test tests --disable=no-member
 cd ..
 
 echo "Running lint for rqd/..."
-pip install ./rqd
+pip install ./rqd[test] --user
 cd rqd
 python -m pylint --rcfile=../ci/pylintrc_main rqd
 python -m pylint --rcfile=../ci/pylintrc_test tests
