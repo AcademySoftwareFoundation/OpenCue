10c426a3471fd4ef0b4bcca4b0e957e24dea1d03
Jimmy Christensen <lithorus@gmail.com>
2025-03-17 22:59:32 +0100

    Update

--- sandbox/install-client-sources.sh
+++ sandbox/install-client-sources.sh
@@ -2,15 +2,5 @@
 
 set -e
 
-pip install -r requirements.txt -r requirements_gui.txt
-
-# Compile the proto used to communicate with the Cuebot server.
-cd proto
-python -m grpc_tools.protoc -I=. \
-  --python_out=../pycue/opencue/compiled_proto \
-  --grpc_python_out=../pycue/opencue/compiled_proto ./*.proto
-cd ..
-python ci/fix_compiled_proto.py pycue/opencue/compiled_proto
-
 # Install all client packages.
 pip install pycue/ pyoutline/ cueadmin/ cuesubmit/ cuegui/
