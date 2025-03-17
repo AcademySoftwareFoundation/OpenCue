f0ef47c595fb8b87ad147646350a969113d0d28b
Jimmy Christensen <lithorus@gmail.com>
2025-03-17 20:18:54 +0100

    TEst

--- ci/check_version_bump.py
+++ ci/check_version_bump.py
@@ -31,6 +31,7 @@ def main():
     version_file_updated = False
 
     for changed_file in changed_files:
+        print(changed_file)
         if changed_file == VERSION_FILE:
             version_file_updated = True
 
