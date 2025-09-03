36b131b14ed09eec00a8e79d59f855d2757e7306
Jimmy Christensen <Lithorus@gmail.com>
2025-09-03 22:34:55 +0200

    Update rust/crates/rqd/src/servant/mod.rs
    Co-authored-by: Diego Tavares <dtavares@imageworks.com>
    Signed-off-by: Jimmy Christensen <Lithorus@gmail.com>

--- rust/crates/rqd/src/servant/mod.rs
+++ rust/crates/rqd/src/servant/mod.rs
@@ -24,28 +24,31 @@ pub async fn serve(
     machine: Arc<MachineImpl>,
     frame_manager: Arc<FrameManager>,
 ) -> Result<()> {
-    let rqd_interface: String = config.grpc.rqd_interface.clone();
-    let mut ip_address: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
-    if !rqd_interface.is_empty() {
-        if let Some(interface) = pnet::datalink::interfaces()
-            .into_iter()
-            .find(|interface| interface.name == rqd_interface)
-        {
-            if let Some(IpNetwork::V4(ipv4_network)) = interface.ips.iter().find(|ip| ip.is_ipv4()) {
-                ip_address = ipv4_network.ip();
-            } else {
-                warn!(
-                    "Could not find an IPv4 address for interface '{}'. Binding to 0.0.0.0",
+    let ip_address: Ipv4Addr = match config.grpc.rqd_interface {
+        None => Ipv4Addr::new(0, 0, 0, 0),
+        Some(rqd_interface) => {
+            let interface = pnet::datalink::interfaces()
+                .into_iter()
+                .find(|interface| interface.name == rqd_interface)
+                .ok_or(tonic::Status::unavailable(format!(
+                    "Could not find network interface '{}'",
                     rqd_interface
-                );
-            }
-        } else {
-            warn!(
-                "Could not find network interface '{}'. Binding to 0.0.0.0",
-                rqd_interface
-            );
+                )))?;
+            // Find V4 interface
+            interface
+                .ips
+                .iter()
+                .find_map(|address| match address {
+                    IpNetwork::V4(a) => Some(a.ip()),
+                    IpNetwork::V6(_) => None, // Unexpected condition
+                })
+                .ok_or(tonic::Status::unavailable(format!(
+                    "Could not find ipv4 network ip for '{}'",
+                    rqd_interface
+                )))?
         }
-    }
+    };
+
     let address: SocketAddr = SocketAddr::new(IpAddr::V4(ip_address), config.grpc.rqd_port);
 
     let running_frame_servant = RunningFrameServant::init(Arc::clone(&frame_manager));
