52718d196b1957e2898db802d69cfd22c28ae42c
abrand <anton.brand@ubisoft.com>
2025-11-05 23:51:13 -0500

    remove builtins imports

--- pyoutline/outline/layer.py
+++ pyoutline/outline/layer.py
@@ -20,8 +20,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from builtins import str
-from builtins import range
 import os
 import sys
 import logging
