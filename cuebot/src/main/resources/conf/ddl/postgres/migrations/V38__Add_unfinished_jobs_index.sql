89b2f8202ae6d0f3fbf1503d448173d50f75a867
Diego Tavares <dtavares@imageworks.com>
2026-03-13 09:36:00 -0700

    Rename index to match the remaining indexes

--- cuebot/src/main/resources/conf/ddl/postgres/migrations/V38__Add_unfinished_jobs_index.sql
+++ cuebot/src/main/resources/conf/ddl/postgres/migrations/V38__Add_unfinished_jobs_index.sql
@@ -1,2 +1,2 @@
 -- Create an index on pk_job for jobs that are not finished
-CREATE INDEX idx_job_active_pkjob ON job(pk_job) WHERE str_state <> 'FINISHED';
+CREATE INDEX i_job_active_pkjob ON job(pk_job) WHERE str_state <> 'FINISHED';
