c6cf3c84c9847d29e592356047656402d93ffa49
Jimmy Christensen <lithorus@gmail.com>
2026-09-21 22:44:47 +0200

    Update backend module references to match renamed identifiers

--- pyoutline/outline/backend.py
+++ pyoutline/outline/backend.py
@@ -21,9 +21,9 @@ after all, and could be used in any number of contexts.
 To this end, PyOutline supports launching jobs on any number of "backends", i.e. the system
 responsible for processing the job -- launching the job / frames, storing job state, etc.
 
-The main backend of course is OpenCue (`outline.backend.cue`), which launches the job on OpenCue.
+The main backend of course is OpenCue (`outline_backend_cue`), which launches the job on OpenCue.
 However this can be extended to support any job management system. We also include a "local"
-backend (`outline.backend.local`) which just runs the job on the current machine, using a SQLite
+backend (`outline_backend_local`) which just runs the job on the current machine, using a SQLite
 database for storing state.
 """
 
