49b01cd17fbe17df7a3a6dd418bd81ff45396114
Jimmy Christensen <lithorus@gmail.com>
2026-09-24 20:22:36 +0200

    Simplify backend serialization logic in `cuerun.py` by directly invoking `backend.serialize` and `backend.serialize_simple`.

--- pyoutline/outline/cuerun.py
+++ pyoutline/outline/cuerun.py
@@ -226,12 +226,8 @@ class OutlineLauncher(object):
         if self.__outline.get_mode() < constants.OUTLINE_MODE_SETUP:
             self.setup()
         if use_pycuerun:
-            return getattr(self.__get_backend_module(),
-                           "serialize",
-                           backend.serialize)(self)
-        return getattr(self.__get_backend_module(),
-                       "serialize_simple",
-                       backend.serialize_simple)(self)
+            return backend.serialize(self)
+        return backend.serialize_simple(self)
 
     def __get_backend_module(self):
         if self.__backend is None:
